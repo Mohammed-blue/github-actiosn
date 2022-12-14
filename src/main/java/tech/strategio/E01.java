6f959cd40f76185d8060e2c3a5b1fe161899b55b
Mohammed Mansour <mohammed0302093@gmail.com>
2022-12-14 11:54:28 -0500

    dsa

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -13,6 +13,29 @@ public class E01 {
      */
     public boolean containsDuplicate(int[] nums) {
         // @TODO: Write out the method
+        // This takes O(N^2) quadric time, at the cost of a constant space.
+//        if (nums.length < 2) {
+//            return false;
+//        }
+//
+//        for (int i = 1; i < nums.length; i++) {
+//            for (int j = 0; j < i; j++) {
+//                if (nums[i] == nums[j]) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+
+        //  Hash Set. The complexity will be O(N) as we only need to conduct a linear scan. However, the space
+        //  requirement is O(N) as we are using a Hash Set that complexity grows linear with the data set.
+        Set<Integer> set = new HashSet<>();
+        for (Integer x : nums) {
+            if (set.contains(x)) {
+                return true;
+            }
+            set.add(x);
+        }
         return false;
     }
 }
